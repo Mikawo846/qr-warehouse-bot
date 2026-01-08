3130be5d4d2e7aef8754410d3df96cabc3afa9f1
Mikawo846 <yashin_mihail@bk.ru>
2026-01-08 15:13:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ from werkzeug.utils import secure_filename
 load_dotenv()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qr_warehouse.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
 
 # Конфигурация CORS для доступа с GitHub Pages
 CORS(app, origins=["https://mikawo846.github.io"])
